5aed2f9d5757ade0f3a88188796e74acefc56f77
R4GN4R <jimchr2001@gmail.com>
2020-01-20 11:33:44 -0500

    Update Line Count_Programming_Challenge.cc

--- Line Count_Programming_Challenge.cc
+++ Line Count_Programming_Challenge.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 
   int numLines = 0;
-  ifstream in("file.txt");
+  ifstream in("file.txt"); // our file name is file.txt
   std::string unused;
   while ( std::getline(in, unused) )
     ++numLines;
